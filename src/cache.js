17de8f1e0ee34e29d9ffd88dc4338e4f4739501c
sabadoryo <eabilbay@gmail.com>
2022-09-13 11:59:20 +0600

    fixes

--- src/cache.js
+++ src/cache.js
@@ -1,6 +1,6 @@
 const redis = require('redis')
 
-const client = redis.createClient();
+const client = redis.createClient(process.env.CACHE_PORT, process.env.CACHE_HOST);
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 
