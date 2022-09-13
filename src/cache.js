dcebe2cabe5efb176bd4f5b48ab09bde2b914fd1
sabadoryo <eabilbay@gmail.com>
2022-09-13 12:04:17 +0600

    asd

--- src/cache.js
+++ src/cache.js
@@ -1,6 +1,8 @@
 const redis = require('redis')
 
-const client = redis.createClient(process.env.CACHE_PORT, process.env.CACHE_HOST);
+const client = redis.createClient(process.env.CACHE_PORT || 6379, process.env.CACHE_HOST, {
+    no_ready_check: true
+});
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 
