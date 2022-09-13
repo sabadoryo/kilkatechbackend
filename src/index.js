c86bfa0f139c6133157d0cd7b378395ee785fdbe
sabadoryo <eabilbay@gmail.com>
2022-09-13 11:57:07 +0600

    added cache

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ require('dotenv').config();
 const http = require('http');
 const app = require('./app');
 const PORT = process.env.PORT || 8000;
+const cache = require('./cache')
 
 const server = http.createServer(app);
 
 async function startServer() {
   server.listen(PORT, () => {
+    cache.connect();
     console.log(`Listening on port ${PORT}...`);
   });
 }
